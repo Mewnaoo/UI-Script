bdd8709945ad01ba0331cb7e563faa6e5ad210e3
Naoo <mewber.rr@gmail.com>
2025-10-10 21:28:46 +0700

    Upd

--- Flags.lua
+++ Flags.lua
@@ -3,7 +3,7 @@ local VirtualUser = game:GetService("VirtualUser")
 
 local FlagsManager = {}
 
-FlagsManager.Folder = "มิวฮับ"
+FlagsManager.Folder = "MewHUB"
 FlagsManager.Ignore = {}
 FlagsManager.Flags = {}
 FlagsManager.Library = nil
@@ -12,7 +12,7 @@ FlagsManager.AntiAFK = {
     Connection = nil
 }
 
--- Simple Enhanced Avantrix Loader Animation
+-- Simple Enhanced MewHUB Loader Animation
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
 local Lighting = game:GetService("Lighting")
@@ -24,7 +24,7 @@ blur.Size = 0
 TweenService:Create(blur, TweenInfo.new(0.6, Enum.EasingStyle.Quart, Enum.EasingDirection.Out), {Size = 28}):Play()
 
 local screenGui = Instance.new("ScreenGui", player:WaitForChild("PlayerGui"))
-screenGui.Name = "AvantrixLoader"
+screenGui.Name = "MewHUBLoader"
 screenGui.ResetOnSpawn = false
 screenGui.IgnoreGuiInset = true
 
@@ -90,7 +90,7 @@ task.spawn(function()
     end
 end)
 
-local word = "Avantrix"
+local word = "มิวฮับ"
 local letters = {}
 
 local function tweenOutAndDestroy()
